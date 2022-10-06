4b99d75a65c4143fddd16a453873a77518d4b74a
Marcus Feitosa <marcusfeitosa02@gmail.com>
2022-10-06 18:11:51 -0300

    cart & addProduct

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -35,9 +35,33 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
       const updatedCart  = [...cart];
-      const productsExists = updatedCart.find(product=> product.id === productId);
-      const stock = await api.get('/stock/${productId}');
+      const productsExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get('/stock/'+productId);
       const stockAmount = stock.data.amount;
+      const currentAmount = productsExists ? productsExists.amount : 0;
+      const amount = currentAmount +1 ;
+
+      if(amount>stockAmount){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+      if (productsExists){
+        productsExists.amount = amount;
+      }else{
+        const product = await api.get('/products/'+productId);
+
+        const newProduct = {
+          ...product.data,
+          amount : 1
+        }
+        updatedCart.push(newProduct);
+      }
+
+      setCart (updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+
+
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -45,9 +69,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      //TODO
     } catch {
-      // TODO
+      //TODO
     }
   };
 
@@ -56,9 +80,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      //TODO
     } catch {
-      // TODO
+      //TODO
     }
   };
 
